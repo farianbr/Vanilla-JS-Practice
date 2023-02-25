e9af3fca7f1ddba72f05e4c54f2805f2f1a1623c
Farian Bin Rahman <farianrahman1000@gmail.com>
2023-02-25 22:53:21 +0600

    first commit

---
+++ eventHandler.js
@@ -0,0 +1,42 @@
+const btn = document.querySelector(".btn")
+const button = document.querySelector(".button")
+const list = document.querySelector('.list')
+const nameInput = document.querySelector('#name')
+const emailInput = document.querySelector('#email')
+const clear = document.querySelector('.clear')
+
+
+nameInput.addEventListener('keyup', function(){
+    emailInput.addEventListener('keyup', function(){
+        if(nameInput.value != '' && emailInput.value != ''){
+            button.removeAttribute('disabled')
+        }
+    })
+
+    if(nameInput.value != '' && emailInput.value != ''){
+        button.removeAttribute('disabled')
+    }  
+})
+
+function onSubmit(e){
+    e.preventDefault()
+
+    const li = document.createElement('li')
+    const name = nameInput.value
+    const email = emailInput.value
+
+    li.appendChild(document.createTextNode(`${name} : ${email}`))
+    list.appendChild(li)
+
+    console.log(`Added Name:${name}, Email:${email}`)
+    nameInput.value = ''
+    emailInput.value = '' 
+    button.setAttribute('disabled', true)
+}
+
+button.addEventListener("click", onSubmit)
+clear.addEventListener("click", function(e){
+    e.preventDefault()
+    list.innerHTML = ''
+    console.log('Cleared!')
+})
